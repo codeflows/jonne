91d0f2e5273080d93a121defcd1a3c12568ad9ff
Jari Aarniala <jari.aarniala@reaktor.com>
2018-02-05 17:16:09 +0200

    Simplify

--- rel/config.exs
+++ rel/config.exs
@@ -2,16 +2,11 @@ use Mix.Releases.Config,
   default_release: :default,
   default_environment: Mix.env()
 
-environment :dev do
-  set dev_mode: true
+environment :default do
+  set dev_mode: false
   set include_erts: false
-  set cookie: :"|JDRG&s%8XCRS%L~%C~*XcuAT~6R9i/JeviJ}mS53Gu)XjR=&(8,9iI6$Q];:,c&"
-end
-
-environment :prod do
-  set include_erts: true
   set include_src: false
-  set cookie: :"3}AujsnJh}bkQ0weXq?GECpd)Rl5G@uO(:_AJ[]K,IKeCL,}9%)<RrX8=w,</(8]"
+  set cookie: :"bad hombre"
 end
 
 release :jonne do
