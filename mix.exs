da9752f927970c2fa964acce8a0fbaa16dff1cba
Jari Aarniala <jari.aarniala@reaktor.com>
2018-02-06 14:00:24 +0200

    Elixir 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Jonne.Mixfile do
     [
       app: :jonne,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
